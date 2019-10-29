58d2cef1852dbd6a18ccf276ec36582475475daa
amreo <amreo@live.it>
2019-10-29 10:09:23 +0100

    Added getTagsOfDatabase endpoint

---
+++ src/main/java/io/ercole/model/DatabaseTagAssociation.java
@@ -0,0 +1,66 @@
+// Copyright (c) 2019 Sorint.lab S.p.A.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package io.ercole.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class DatabaseTagAssociation {
+
+    /** The id. */
+	@Id
+    private long id;
+
+    private String hostname;
+    private String dbname;
+    private String tag;
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(final String tag) {
+        this.tag = tag;
+    }
+
+    public String getDbname() {
+        return dbname;
+    }
+
+    public void setDbname(final String dbname) {
+        this.dbname = dbname;
+    }
+
+    public void setHostname(final String hostname) {
+        this.hostname = hostname;
+    }
+
+    public DatabaseTagAssociation() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(final long id) {
+        this.id = id;
+    }
+}
---
+++ src/main/java/io/ercole/repositories/DatabaseTagAssociationRepository.java
@@ -0,0 +1,26 @@
+// Copyright (c) 2019 Sorint.lab S.p.A.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package io.ercole.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import io.ercole.model.DatabaseTagAssociation;
+
+public interface DatabaseTagAssociationRepository extends PagingAndSortingRepository<DatabaseTagAssociation, Long> {
+    List<DatabaseTagAssociation> findByHostnameAndDbname(String hostname, String dbname);
+}
