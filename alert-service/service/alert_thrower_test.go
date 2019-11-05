54d5cf5917d844102f3e714518d0a48a34f3460c
amreo@live.it <amreo>
2019-11-05 16:28:46 +0100

    Moved common test code to common_test.go

--- alert-service/service/alert_thrower_test.go
+++ alert-service/service/alert_thrower_test.go
@@ -1,11 +1,7 @@
 package service
 
 import (
-	"errors"
 	"testing"
-	"time"
-
-	"github.com/amreo/ercole-services/utils"
 
 	"github.com/stretchr/testify/require"
 
@@ -13,25 +9,6 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 )
-
-//go:generate mockgen -source ../database/database.go -destination=fake_database.go -package=service
-
-//Common data
-var errMock error = errors.New("MockError")
-var aerrMock utils.AdvancedErrorInterface = utils.NewAdvancedErrorPtr(errMock, "mock")
-
-//p parse the string s and return the equivalent time
-func p(s string) time.Time {
-	t, _ := time.Parse(time.RFC3339, s)
-	return t
-}
-
-//btc break the time continuum and return a function that return the time t
-func btc(t time.Time) func() time.Time {
-	return func() time.Time {
-		return t
-	}
-}
 
 //ThrowNewDatabaseAlert tests
 
---
+++ alert-service/service/common_test.go
@@ -0,0 +1,27 @@
+package service
+
+import (
+	"errors"
+	"time"
+
+	"github.com/amreo/ercole-services/utils"
+)
+
+//go:generate mockgen -source ../database/database.go -destination=fake_database.go -package=service
+
+//Common data
+var errMock error = errors.New("MockError")
+var aerrMock utils.AdvancedErrorInterface = utils.NewAdvancedErrorPtr(errMock, "mock")
+
+//p parse the string s and return the equivalent time
+func p(s string) time.Time {
+	t, _ := time.Parse(time.RFC3339, s)
+	return t
+}
+
+//btc break the time continuum and return a function that return the time t
+func btc(t time.Time) func() time.Time {
+	return func() time.Time {
+		return t
+	}
+}
