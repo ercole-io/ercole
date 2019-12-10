3c7609e75f628b4bf0dba478eb76879691a72a82
amreo@live.it <amreo>
2019-12-10 14:52:45 +0100

    Moved common code in cmds to common.go

--- cmd/search_clusters.go
+++ cmd/search_clusters.go
@@ -15,80 +15,16 @@ limitations under the License.
 */
 package cmd
 
-import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"net/url"
-	"os"
-	"strconv"
-	"strings"
-
-	"github.com/amreo/ercole-services/utils"
-
-	"github.com/spf13/cobra"
-)
-
-// searchClustersCmd represents the search-clusters command
-var searchClustersCmd = &cobra.Command{
-	Use:   "search-clusters",
-	Short: "Search current clusters",
-	Long:  `search-clusters search the most matching clusters to the arguments`,
-	Run: func(cmd *cobra.Command, args []string) {
-		params := url.Values{
-			"full":        []string{strconv.FormatBool(!summary)},
-			"search":      []string{strings.Join(args, " ")},
-			"location":    []string{location},
-			"environment": []string{environment},
-		}
-
-		if sortBy != "" {
-			params.Set("sort-by", sortBy)
-			params.Set("sort-desc", strconv.FormatBool(sortDesc))
-		}
-
-		resp, err := http.Get(
-			utils.NewAPIUrl(
-				ercoleConfig.APIService.RemoteEndpoint,
-				ercoleConfig.APIService.UserUsername,
-				ercoleConfig.APIService.UserPassword,
-				"/clusters",
-				params,
-			).String())
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Failed to search clusters data: %v\n", err)
-			os.Exit(1)
-		} else if resp.StatusCode < 200 || resp.StatusCode > 299 {
-			out, _ := ioutil.ReadAll(resp.Body)
-			defer resp.Body.Close()
-			fmt.Fprintf(os.Stderr, "Failed to search clusters data(Status: %d): %s\n", resp.StatusCode, string(out))
-			os.Exit(1)
-		} else {
-			out, _ := ioutil.ReadAll(resp.Body)
-			defer resp.Body.Close()
-			var res []interface{}
-			err = json.Unmarshal(out, &res)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Failed to unmarshal response body: %v (%s)\n", err, string(out))
-				os.Exit(1)
-			}
-
-			for _, item := range res {
-				enc := json.NewEncoder(os.Stdout)
-				enc.SetIndent("", "    ")
-				enc.Encode(item)
-			}
-		}
-
-	},
-}
-
 func init() {
+
+	searchClustersCmd := simpleAPIRequestCommand("search-clusters",
+		"Search current clusters",
+		`search-clusters search the most matching clusters to the arguments`,
+		true, true, false, true, true, true,
+		"/clusters",
+		"Failed to search clusters data: %v\n",
+		"Failed to search clusters data(Status: %d): %s\n",
+	)
+
 	apiCmd.AddCommand(searchClustersCmd)
-	searchClustersCmd.Flags().BoolVarP(&summary, "summary", "s", false, "Summary mode")
-	searchClustersCmd.Flags().StringVar(&sortBy, "sort-by", "", "Sort by field")
-	searchClustersCmd.Flags().BoolVar(&sortDesc, "desc-order", false, "Sort descending")
-	searchClustersCmd.Flags().StringVarP(&location, "location", "l", "", "Filter by location")
-	searchClustersCmd.Flags().StringVarP(&environment, "environment", "e", "", "Filter by environment")
 }
--- cmd/search_segment_advisors.go
+++ cmd/search_segment_advisors.go
@@ -15,78 +15,15 @@ limitations under the License.
 */
 package cmd
 
-import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"net/url"
-	"os"
-	"strconv"
-	"strings"
-
-	"github.com/amreo/ercole-services/utils"
-
-	"github.com/spf13/cobra"
-)
-
-// searchSegmentAdvisorsCmd represents the search-segment-advisors command
-var searchSegmentAdvisorsCmd = &cobra.Command{
-	Use:   "search-segment-advisors",
-	Short: "Search current segment advisors",
-	Long:  `search-segment-advisors search the most matching segment advisors to the arguments`,
-	Run: func(cmd *cobra.Command, args []string) {
-		params := url.Values{
-			"search":      []string{strings.Join(args, " ")},
-			"location":    []string{location},
-			"environment": []string{environment},
-		}
-
-		if sortBy != "" {
-			params.Set("sort-by", sortBy)
-			params.Set("sort-desc", strconv.FormatBool(sortDesc))
-		}
-
-		resp, err := http.Get(
-			utils.NewAPIUrl(
-				ercoleConfig.APIService.RemoteEndpoint,
-				ercoleConfig.APIService.UserUsername,
-				ercoleConfig.APIService.UserPassword,
-				"/segment-advisors",
-				params,
-			).String())
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Failed to search segment advisors data: %v\n", err)
-			os.Exit(1)
-		} else if resp.StatusCode < 200 || resp.StatusCode > 299 {
-			out, _ := ioutil.ReadAll(resp.Body)
-			defer resp.Body.Close()
-			fmt.Fprintf(os.Stderr, "Failed to search segment advisors data(Status: %d): %s\n", resp.StatusCode, string(out))
-			os.Exit(1)
-		} else {
-			out, _ := ioutil.ReadAll(resp.Body)
-			defer resp.Body.Close()
-			var res []interface{}
-			err = json.Unmarshal(out, &res)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "Failed to unmarshal response body: %v (%s)\n", err, string(out))
-				os.Exit(1)
-			}
-
-			for _, item := range res {
-				enc := json.NewEncoder(os.Stdout)
-				enc.SetIndent("", "    ")
-				enc.Encode(item)
-			}
-		}
-
-	},
-}
-
 func init() {
+	searchSegmentAdvisorsCmd := simpleAPIRequestCommand("search-segment-advisors",
+		"Search current segment advisors",
+		`search-segment-advisors search the most matching segment advisors to the arguments`,
+		false, false, false, true, true, true,
+		"/segment-advisors",
+		"Failed to search segment advisors data: %v\n",
+		"Failed to search segment advisors data(Status: %d): %s\n",
+	)
+
 	apiCmd.AddCommand(searchSegmentAdvisorsCmd)
-	searchSegmentAdvisorsCmd.Flags().StringVar(&sortBy, "sort-by", "", "Sort by field")
-	searchSegmentAdvisorsCmd.Flags().BoolVar(&sortDesc, "desc-order", false, "Sort descending")
-	searchSegmentAdvisorsCmd.Flags().StringVarP(&location, "location", "l", "", "Filter by location")
-	searchSegmentAdvisorsCmd.Flags().StringVarP(&environment, "environment", "e", "", "Filter by environment")
 }
