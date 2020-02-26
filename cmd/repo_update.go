cb8df212c457d0260c57d0b0e2f23d911e7ac02d
amreo <amreo@live.it>
2020-02-26 18:53:50 +0100

    Added update cmd

---
+++ cmd/repo_update.go
@@ -0,0 +1,51 @@
+// Copyright (c) 2019 Sorint.lab S.p.A.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	repoUpdateCmd := &cobra.Command{
+		Use:   "update",
+		Short: "Update all artifacts installed",
+		Long:  `Install the most recent version of all installed artifacts`,
+		Run: func(cmd *cobra.Command, args []string) {
+			//Get the list of the repository
+			index := readOrUpdateIndex()
+
+			updateCandidates := make(map[*artifactInfo]bool)
+
+			//Search the artifact and install it for every artifact
+			for _, art := range index {
+				f := index.searchArtifactByRepositoryAndName(art.Repository, art.Name)
+
+				if !f.Installed {
+					updateCandidates[f] = true
+				}
+			}
+
+			//Install all updateCandidates
+			for art, _ := range updateCandidates {
+				art.Install(art)
+			}
+		},
+	}
+	repoUpdateCmd.Flags().BoolVarP(&verbose, "verbose", "v", false, "Verbose")
+
+	repoCmd.AddCommand(repoUpdateCmd)
+}
