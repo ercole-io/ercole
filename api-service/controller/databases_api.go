071cb6c179e522d8d988e90220181a840920c6be
Fabrizio Cirelli <cirelli94@gmail.com>
2020-03-04 12:45:56 +0100

    Renaming and refactoring

--- api-service/controller/databases_api.go
+++ api-service/controller/databases_api.go
@@ -663,14 +663,3 @@ func (ctrl *APIController) SetLicenseCount(w http.ResponseWriter, r *http.Reques
 	//Write the data
 	utils.WriteJSONResponse(w, http.StatusOK, nil)
 }
-
-// GetDefaultDatabasesTags return the default list of database tags from configuration
-func (ctrl *APIController) GetDefaultDatabasesTags(w http.ResponseWriter, r *http.Request) {
-	tags, err := ctrl.Service.GetDefaultDatabasesTags()
-	if err != nil {
-		utils.WriteAndLogError(ctrl.Log, w, http.StatusInternalServerError, err)
-		return
-	}
-
-	utils.WriteJSONResponse(w, http.StatusOK, tags)
-}
