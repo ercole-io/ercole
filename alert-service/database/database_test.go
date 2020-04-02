135d49bf039b3448c6390b42e29591e8e6a58a72
Fabrizio Cirelli <cirelli94@gmail.com>
2020-04-02 17:20:42 +0200

    Added TestFindOldCurrentHosts tests

--- alert-service/database/database_test.go
+++ alert-service/database/database_test.go
@@ -152,6 +152,46 @@ func (m *MongodbSuite) TestInsertAlert_Success() {
 	assert.Equal(m.T(), alert1, out)
 }
 
+func (m *MongodbSuite) TestFindOldCurrentHosts() {
+	defer m.db.Client.Database(m.dbname).Collection("hosts").DeleteMany(context.TODO(), bson.M{})
+
+	hd := utils.LoadFixtureHostData(m.T(), "../../fixture/test_dataservice_mongohostdata_01.json")
+	hd.ID = primitive.NewObjectIDFromTimestamp(time.Now())
+	hd.CreatedAt = utils.P("2019-11-05T14:02:03Z")
+	err := m.InsertHostData(hd)
+	require.NoError(m.T(), err)
+
+	hd2 := utils.LoadFixtureHostData(m.T(), "../../fixture/test_dataservice_mongohostdata_03.json")
+	hd2.ID = primitive.NewObjectIDFromTimestamp(time.Now())
+	hd2.CreatedAt = utils.P("2020-01-05T14:02:03Z")
+	err2 := m.InsertHostData(hd2)
+	require.NoError(m.T(), err2)
+
+	m.T().Run("Should not find any", func(t *testing.T) {
+		hosts, err := m.db.FindOldCurrentHosts(utils.P("2019-10-05T14:02:03Z"))
+		require.NoError(m.T(), err)
+		assert.Empty(m.T(), hosts)
+	})
+
+	m.T().Run("Should find one", func(t *testing.T) {
+		hosts, err := m.db.FindOldCurrentHosts(utils.P("2019-12-05T14:02:03Z"))
+		require.NoError(m.T(), err)
+
+		assert.Len(m.T(), hosts, 1)
+		expectedHosts := append(make([]string, 0), hd.Info.Hostname)
+		assert.ElementsMatch(m.T(), expectedHosts, hosts)
+	})
+
+	m.T().Run("Should find two", func(t *testing.T) {
+		hosts, err := m.db.FindOldCurrentHosts(utils.P("2020-02-05T14:02:03Z"))
+		require.NoError(m.T(), err)
+
+		assert.Len(m.T(), hosts, 2)
+		expectedHosts := append(make([]string, 0), hd.Info.Hostname, hd2.Info.Hostname)
+		assert.ElementsMatch(m.T(), expectedHosts, hosts)
+	})
+}
+
 func (m *MongodbSuite) TestExistNoDataAlert_SuccessNotExist() {
 	_, err := m.db.InsertAlert(alert1)
 	defer m.db.Client.Database(m.dbname).Collection("alerts").DeleteMany(context.TODO(), bson.M{})
