2c66ab8d2cb52353158fb91862809ad79821fbea
amreo <amreo@live.it>
2020-04-15 09:57:05 +0200

    Added tests for SearchAlerts

---
+++ api-service/controller/common_test.go
@@ -0,0 +1,28 @@
+// Copyright (c) 2019 Sorint.lab S.p.A.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package controller
+
+import (
+	"errors"
+
+	"github.com/amreo/ercole-services/utils"
+)
+
+//go:generate mockgen -source ../service/service.go -destination=fake_service.go -package=controller
+
+//Common data
+var errMock error = errors.New("MockError")
+var aerrMock utils.AdvancedErrorInterface = utils.NewAdvancedErrorPtr(errMock, "mock")
