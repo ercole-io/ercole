798d5cd73c1242d12f5d8cf758c9a6ce653dd771
Fabrizio Cirelli <cirelli94@gmail.com>
2020-05-13 10:21:22 +0200

    Changed organization docker push

--- docker_push.sh
+++ docker_push.sh
@@ -2,4 +2,4 @@
 
 docker tag ercole/ercole-services ercole/ercole-services:${VERSION}
 [[ $? == 0 ]] && echo "$(echo ${DOCKER_PASSWORD} | base64 -d)" | docker login --username "$(echo ${DOCKER_USERNAME} | base64 -d)" --password-stdin
-[[ $? == 0 ]] && docker push ercole/ercole-services:${VERSION}
+[[ $? == 0 ]] && docker push ercoleorg/ercole-services:${VERSION}
