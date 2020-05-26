11f684b37285363faa15a90410def39466dc2b44
Fabrizio Cirelli <cirelli94@gmail.com>
2020-05-26 14:47:34 +0200

    Fixed curl

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ docker tag sorintlab/ercole-services sorintlab/ercole-services:${VERSION}
 [[ $? == 0 ]] && echo "$(echo ${DOCKER_PASSWORD} | base64 -d)" | docker login --username "$(echo ${DOCKER_USERNAME} | base64 -d)" --password-stdin
 [[ $? == 0 ]] && docker push sorintlab/ercole-services:${VERSION}
 
-[[ $? == 0 ]] && curl --location --request POST '${REDEPLOY_URL}' --header 'Content-Type: application/json' --data-raw '{ "namespace": "ercole", "podname" : "ercole-services" }'
+[[ $? == 0 ]] && curl --location --request POST ${REDEPLOY_URL} --header 'Content-Type: application/json' --data-raw '{ "namespace": "ercole", "podname" : "ercole-services" }'
