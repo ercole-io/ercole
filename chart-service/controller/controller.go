980991310139bf174aea70de6181975d8118cc7d
amreo <amreo0x+github@protonmail.com>
2020-07-06 10:32:48 +0200

    Added empty chart service

---
+++ chart-service/controller/controller.go
@@ -0,0 +1,43 @@
+// Copyright (c) 2020 Sorint.lab S.p.A.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package controller
+
+import (
+	"time"
+
+	"github.com/ercole-io/ercole/api-service/auth"
+	"github.com/ercole-io/ercole/chart-service/service"
+
+	"github.com/ercole-io/ercole/config"
+	"github.com/sirupsen/logrus"
+)
+
+// ChartControllerInterface is a interface that wrap methods used to querying data
+type ChartControllerInterface interface{}
+
+// ChartController is the struct used to handle the requests from agents and contains the concrete implementation of ChartControllerInterface
+type ChartController struct {
+	// Config contains the dataservice global configuration
+	Config config.Configuration
+	// Service contains the underlying service used to perform various logical and store operations
+	Service service.ChartServiceInterface
+	// TimeNow contains a function that return the current time
+	TimeNow func() time.Time
+	// Log contains logger formatted
+	Log *logrus.Logger
+	// Authenticator contains the authenticator
+	Authenticator auth.AuthenticationProvider
+}
