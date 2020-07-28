f555e885643ab5d3c7c13148576a74916452da2b
amreo <amreo0x+github@protonmail.com>
2020-07-28 14:05:14 +0200

    Fixed data types

--- model/microsoft_sqlserver_database_schema.go
+++ model/microsoft_sqlserver_database_schema.go
@@ -68,11 +68,11 @@ var MicrosoftSQLServerDatabaseSchemaBsonValidatorRules = bson.M{
 			},
 		},
 		"usedSpace": bson.M{
-			"bsonType": "double",
+			"bsonType": "number",
 			"minimum":  0,
 		},
 		"allocatedSpace": bson.M{
-			"bsonType": "double",
+			"bsonType": "number",
 			"minimum":  0,
 		},
 	},
