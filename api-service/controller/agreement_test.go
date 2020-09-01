45e583929ad6bf1f86fef4737fd0f5133a7c23fc
amreo <amreo0x+github@protonmail.com>
2020-09-01 15:29:56 +0200

    Added GET /agreements/oracle/database endpoint

--- api-service/controller/agreement_test.go
+++ api-service/controller/agreement_test.go
@@ -227,3 +227,194 @@ func TestAddOracleDatabaseAgreements_InternalServerError(t *testing.T) {
 
 	require.Equal(t, http.StatusInternalServerError, rr.Code)
 }
+
+func TestSearchOracleDatabaseAgreements_Success(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	as := NewMockAPIServiceInterface(mockCtrl)
+	ac := APIController{
+		TimeNow: utils.Btc(utils.P("2019-11-05T14:02:03Z")),
+		Service: as,
+		Config:  config.Configuration{},
+		Log:     utils.NewLogger("TEST"),
+	}
+
+	expectedRes := []apimodel.OracleDatabaseAgreementsFE{
+		{
+			ItemDescription: "foobar",
+		},
+	}
+
+	as.EXPECT().
+		SearchOracleDatabaseAgreements("", apimodel.SearchOracleDatabaseAgreementsFilters{
+			Unlimited:         "true",
+			CatchAll:          "NULL",
+			AvailableCountGTE: -1,
+			AvailableCountLTE: -1,
+			LicensesCountGTE:  -1,
+			LicensesCountLTE:  -1,
+			UsersCountGTE:     -1,
+			UsersCountLTE:     -1,
+		}).
+		Return(expectedRes, nil)
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(ac.SearchOracleDatabaseAgreements)
+	req, err := http.NewRequest("GET", "/?unlimited=true", nil)
+	require.NoError(t, err)
+
+	handler.ServeHTTP(rr, req)
+
+	require.Equal(t, http.StatusOK, rr.Code)
+	assert.JSONEq(t, utils.ToJSON(expectedRes), rr.Body.String())
+}
+
+func TestSearchOracleDatabaseAgreements_FailedUnprocessableEntity(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	as := NewMockAPIServiceInterface(mockCtrl)
+	ac := APIController{
+		TimeNow: utils.Btc(utils.P("2019-11-05T14:02:03Z")),
+		Service: as,
+		Config:  config.Configuration{},
+		Log:     utils.NewLogger("TEST"),
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(ac.SearchOracleDatabaseAgreements)
+	req, err := http.NewRequest("GET", "/?unlimited=sasasd", nil)
+	require.NoError(t, err)
+
+	handler.ServeHTTP(rr, req)
+
+	require.Equal(t, http.StatusUnprocessableEntity, rr.Code)
+}
+
+func TestSearchOracleDatabaseAgreements_FailedInternalServerError(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	as := NewMockAPIServiceInterface(mockCtrl)
+	ac := APIController{
+		TimeNow: utils.Btc(utils.P("2019-11-05T14:02:03Z")),
+		Service: as,
+		Config:  config.Configuration{},
+		Log:     utils.NewLogger("TEST"),
+	}
+
+	as.EXPECT().
+		SearchOracleDatabaseAgreements("", apimodel.SearchOracleDatabaseAgreementsFilters{
+			Unlimited:         "NULL",
+			CatchAll:          "NULL",
+			AvailableCountGTE: -1,
+			AvailableCountLTE: -1,
+			LicensesCountGTE:  -1,
+			LicensesCountLTE:  -1,
+			UsersCountGTE:     -1,
+			UsersCountLTE:     -1,
+		}).
+		Return(nil, aerrMock)
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(ac.SearchOracleDatabaseAgreements)
+	req, err := http.NewRequest("GET", "/", nil)
+	require.NoError(t, err)
+
+	handler.ServeHTTP(rr, req)
+
+	require.Equal(t, http.StatusInternalServerError, rr.Code)
+}
+
+func TestGetSearchOracleDatabaseAgreementsFilters_SuccessEmpty(t *testing.T) {
+	r, _ := http.NewRequest("GET", "/", nil)
+	filters, err := GetSearchOracleDatabaseAgreementsFilters(r)
+	require.NoError(t, err)
+	assert.Equal(t, apimodel.SearchOracleDatabaseAgreementsFilters{
+		CatchAll:          "NULL",
+		Unlimited:         "NULL",
+		AvailableCountGTE: -1,
+		AvailableCountLTE: -1,
+		LicensesCountGTE:  -1,
+		LicensesCountLTE:  -1,
+		UsersCountGTE:     -1,
+		UsersCountLTE:     -1,
+	}, filters)
+}
+
+func TestGetSearchOracleDatabaseAgreementsFilters_SuccessFull(t *testing.T) {
+	r, err := http.NewRequest("GET", "/?agreement-id=foo&part-id=bar&item-description=boz&csi=pippo&metrics=pluto&reference-number=foobar&unlimited=false&catch-all=true&licenses-count-lte=10&licenses-count-gte=20&users-count-lte=5&users-count-gte=15&available-count-lte=3&available-count-gte=13", nil)
+	require.NoError(t, err)
+	filters, err := GetSearchOracleDatabaseAgreementsFilters(r)
+	require.NoError(t, err)
+	assert.Equal(t, apimodel.SearchOracleDatabaseAgreementsFilters{
+		AgreementID:       "foo",
+		PartID:            "bar",
+		ItemDescription:   "boz",
+		CSI:               "pippo",
+		Metrics:           "pluto",
+		ReferenceNumber:   "foobar",
+		Unlimited:         "false",
+		CatchAll:          "true",
+		LicensesCountLTE:  10,
+		LicensesCountGTE:  20,
+		UsersCountLTE:     5,
+		UsersCountGTE:     15,
+		AvailableCountLTE: 3,
+		AvailableCountGTE: 13,
+	}, filters)
+}
+
+func TestGetSearchOracleDatabaseAgreementsFilters_Fail1(t *testing.T) {
+	r, err := http.NewRequest("GET", "/?unlimited=asdfasdf", nil)
+	require.NoError(t, err)
+	_, err = GetSearchOracleDatabaseAgreementsFilters(r)
+	require.Error(t, err)
+}
+
+func TestGetSearchOracleDatabaseAgreementsFilters_Fail2(t *testing.T) {
+	r, err := http.NewRequest("GET", "/?catch-all=asdfasdf", nil)
+	require.NoError(t, err)
+	_, err = GetSearchOracleDatabaseAgreementsFilters(r)
+	require.Error(t, err)
+}
+
+func TestGetSearchOracleDatabaseAgreementsFilters_Fail3(t *testing.T) {
+	r, err := http.NewRequest("GET", "/?licenses-count-lte=asdfasdf", nil)
+	require.NoError(t, err)
+	_, err = GetSearchOracleDatabaseAgreementsFilters(r)
+	require.Error(t, err)
+}
+
+func TestGetSearchOracleDatabaseAgreementsFilters_Fail4(t *testing.T) {
+	r, err := http.NewRequest("GET", "/?licenses-count-gte=asdfasdf", nil)
+	require.NoError(t, err)
+	_, err = GetSearchOracleDatabaseAgreementsFilters(r)
+	require.Error(t, err)
+}
+
+func TestGetSearchOracleDatabaseAgreementsFilters_Fail5(t *testing.T) {
+	r, err := http.NewRequest("GET", "/?users-count-lte=asdfasdf", nil)
+	require.NoError(t, err)
+	_, err = GetSearchOracleDatabaseAgreementsFilters(r)
+	require.Error(t, err)
+}
+
+func TestGetSearchOracleDatabaseAgreementsFilters_Fail6(t *testing.T) {
+	r, err := http.NewRequest("GET", "/?users-count-gte=asdfasdf", nil)
+	require.NoError(t, err)
+	_, err = GetSearchOracleDatabaseAgreementsFilters(r)
+	require.Error(t, err)
+}
+
+func TestGetSearchOracleDatabaseAgreementsFilters_Fail7(t *testing.T) {
+	r, err := http.NewRequest("GET", "/?available-count-lte=asdfasdf", nil)
+	require.NoError(t, err)
+	_, err = GetSearchOracleDatabaseAgreementsFilters(r)
+	require.Error(t, err)
+}
+
+func TestGetSearchOracleDatabaseAgreementsFilters_Fail8(t *testing.T) {
+	r, err := http.NewRequest("GET", "/?available-count-gte=asdfasdf", nil)
+	require.NoError(t, err)
+	_, err = GetSearchOracleDatabaseAgreementsFilters(r)
+	require.Error(t, err)
+}
