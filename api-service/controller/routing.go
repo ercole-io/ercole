2f60c5f839210cc9d18e1873346c1920e2f5eacc
amreo <amreo0x+github@protonmail.com>
2020-09-01 15:29:56 +0200

    Added GET /agreements/oracle/database endpoint

--- api-service/controller/routing.go
+++ api-service/controller/routing.go
@@ -91,6 +91,7 @@ func setupProtectedRoutes(router *mux.Router, ctrl APIControllerInterface) {
 	router.HandleFunc("/licenses/{name}/cost-per-processor", ctrl.SetLicenseCostPerProcessor).Methods("PUT")
 	router.HandleFunc("/licenses/{name}/unlimited-status", ctrl.SetLicenseUnlimitedStatus).Methods("PUT")
 	router.HandleFunc("/agreements/oracle/database", ctrl.AddOracleDatabaseAgreements).Methods("POST")
+	router.HandleFunc("/agreements/oracle/database", ctrl.SearchOracleDatabaseAgreements).Methods("GET")
 	router.HandleFunc("/alerts", ctrl.SearchAlerts).Methods("GET")
 	router.HandleFunc("/alerts/{id}", ctrl.AckAlert).Methods("POST")
 
