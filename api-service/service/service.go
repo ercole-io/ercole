c358c262ee578479903b2967cc823802c266e742
amreo <amreo0x+github@protonmail.com>
2020-09-04 11:31:50 +0200

    Removed PUT /licenses

--- api-service/service/service.go
+++ api-service/service/service.go
@@ -146,9 +146,6 @@ type APIServiceInterface interface {
 	// RemoveAssociatedHostToOracleDatabaseAgreement remove the host from the list of associated hosts of the agreement
 	RemoveAssociatedHostToOracleDatabaseAgreement(id primitive.ObjectID, hostname string) utils.AdvancedErrorInterface
 
-	// SetLicensesCount set the count of all licenses in newLicenses
-	// It assumes that newLicenses maps contain the string _id and the int Count
-	SetLicensesCount(newLicenses []map[string]interface{}) utils.AdvancedErrorInterface
 	// SetPatchingFunction set the patching function of a host
 	SetPatchingFunction(hostname string, pf model.PatchingFunction) (interface{}, utils.AdvancedErrorInterface)
 	// DeletePatchingFunction delete the patching function of a host
