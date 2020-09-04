5c77fcd6d5ffae1638a5ad5eb1dd2dd87aa3fb45
amreo <amreo0x+github@protonmail.com>
2020-09-04 11:46:04 +0200

    Removed PUT /licenses/{name}/count endpoint

--- api-service/database/database.go
+++ api-service/database/database.go
@@ -131,8 +131,6 @@ type MongoDatabaseInterface interface {
 	// UpdateOracleDatabaseAgreement update a Oracle/Database agreement in the database
 	UpdateOracleDatabaseAgreement(newAgreement model.OracleDatabaseAgreement) utils.AdvancedErrorInterface
 
-	// SetLicenseCount set the count of a certain license
-	SetLicenseCount(name string, count int) utils.AdvancedErrorInterface
 	// SetLicenseCostPerProcessor set the cost per processor of a certain license
 	SetLicenseCostPerProcessor(name string, costPerProcessor float64) utils.AdvancedErrorInterface
 	// SetLicenseUnlimitedStatus set the unlimited status of a certain license
