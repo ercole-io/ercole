699f425a92f37582761b8758515d7766c46385c6
Fabrizio Cirelli <cirelli94@gmail.com>
2020-10-14 12:08:04 +0200

    Removed LicenseCount and licenses collection

--- cmd/migrate.go
+++ cmd/migrate.go
@@ -17,8 +17,6 @@ package cmd
 
 import (
 	"context"
-	"io/ioutil"
-	"strings"
 
 	"github.com/spf13/cobra"
 
@@ -34,20 +32,8 @@ var migrateCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		log := utils.NewLogger("SERV")
 
-		if ercoleConfig.ResourceFilePath == "RESOURCES_NOT_FOUND" {
-			log.Fatal("The directory for resources wasn't found")
-		}
-
-		//Read initial licenses list
-		content, err := ioutil.ReadFile(ercoleConfig.ResourceFilePath + "/initial_oracle_licenses_list.txt")
-		if err != nil {
-			log.Fatalf("Cannot read the licenses list: %v\n", err)
-		}
-		lines := strings.Split(string(content), "\n")
-
-		//Migrate
 		cl := migration.ConnectToMongodb(log, ercoleConfig.Mongodb)
-		migration.Migrate(log, cl.Database(ercoleConfig.Mongodb.DBName), lines)
+		migration.Migrate(log, cl.Database(ercoleConfig.Mongodb.DBName))
 		cl.Disconnect(context.TODO())
 	},
 }
--- cmd/serve.go
+++ cmd/serve.go
@@ -18,9 +18,7 @@ package cmd
 import (
 	"context"
 	"fmt"
-	"io/ioutil"
 	"net/http"
-	"strings"
 	"sync"
 	"time"
 
@@ -99,17 +97,9 @@ func serve(enableDataService bool,
 	var wg sync.WaitGroup
 
 	if ercoleConfig.Mongodb.Migrate {
-		//Read initial licenses list
-		content, err := ioutil.ReadFile(ercoleConfig.ResourceFilePath + "/initial_oracle_licenses_list.txt")
-		if err != nil {
-			log.Fatalf("Cannot read the licenses list: %v\n", err)
-		}
-		lines := strings.Split(string(content), "\n")
-
-		//Migrate
 		log.Info("Migrating...")
 		cl := migration.ConnectToMongodb(log, ercoleConfig.Mongodb)
-		migration.Migrate(log, cl.Database(ercoleConfig.Mongodb.DBName), lines)
+		migration.Migrate(log, cl.Database(ercoleConfig.Mongodb.DBName))
 		cl.Disconnect(context.TODO())
 	}
 
