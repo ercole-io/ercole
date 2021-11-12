5ff5084ff86bc0d479ad50607406631b33f7c705
cbiglioli <cbiglioli@gmail.com>
2021-11-12 15:15:25 +0100

    Changed file from agreements to licenses

--- api-service/controller/oracle_database_agreements_test.go
+++ api-service/controller/oracle_database_agreements_test.go
@@ -1240,35 +1240,3 @@ func TestGetOracleDatabaseAgreementsXLSX_InternalServerError1(t *testing.T) {
 
 	require.Equal(t, http.StatusInternalServerError, rr.Code)
 }
-
-func TestUpdateLicenseIgnoredField_Success(t *testing.T) {
-	mockCtrl := gomock.NewController(t)
-	defer mockCtrl.Finish()
-	as := NewMockAPIServiceInterface(mockCtrl)
-	ac := APIController{
-		TimeNow: utils.Btc(utils.P("2019-11-05T14:02:03Z")),
-		Service: as,
-		Config: config.Configuration{
-			APIService: config.APIService{
-				ReadOnly: false,
-			},
-		},
-		Log: logger.NewLogger("TEST"),
-	}
-
-	rr := httptest.NewRecorder()
-	handler := http.HandlerFunc(ac.UpdateLicenseIgnoredField)
-	req, err := http.NewRequest("PUT", "/hosts/serv123/databases/TEST123/licenses/A90611/ignored/false", &FailingReader{})
-	req = mux.SetURLVars(req, map[string]string{
-		"hostname":      "serv123",
-		"dbname":        "TEST123",
-		"licenseTypeID": "A90611",
-		"ignored":       "false",
-	})
-	require.NoError(t, err)
-
-	handler.ServeHTTP(rr, req)
-
-	require.Equal(t, http.StatusBadRequest, rr.Code)
-
-}
--- api-service/database/oracle_database_agreements.go
+++ api-service/database/oracle_database_agreements.go
@@ -23,7 +23,6 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"github.com/ercole-io/ercole/v2/api-service/dto"
 	"github.com/ercole-io/ercole/v2/model"
@@ -238,27 +237,4 @@ func (md *MongoDatabase) ListHostUsingOracleDatabaseLicenses() ([]dto.HostUsingO
 	}
 
 	return out, nil
-}
-
-// UpdateLicenseIgnoredField update host ignored field (true/false)
-func (md *MongoDatabase) UpdateLicenseIgnoredField(hostname string, dbname string, licenseTypeID string, ignored bool) error {
-	result, err := md.Client.Database(md.Config.Mongodb.DBName).Collection("hosts").
-		UpdateOne(context.TODO(),
-			bson.M{
-				"hostname": hostname,
-				"archived": false,
-				"features.oracle.database.databases.instanceName":           dbname,
-				"features.oracle.database.databases.licenses.licenseTypeID": licenseTypeID,
-			},
-			bson.M{"$set": bson.M{"features.oracle.database.databases.$[elemDB].licenses.$[elemLic].ignored": ignored}},
-			options.Update().SetArrayFilters(options.ArrayFilters{Filters: []interface{}{bson.M{"elemDB.instanceName": dbname}, bson.M{"elemLic.licenseTypeID": licenseTypeID}}}),
-		)
-	if err != nil {
-		return utils.NewError(err, "DB ERROR")
-	}
-	if result.MatchedCount != 1 {
-		return utils.ErrLicenseNotFound
-	}
-
-	return nil
 }
