ee5a081bfe5537cc22d2e4a2273afc2d705e62ed
cbiglioli <cbiglioli@gmail.com>
2021-12-09 16:06:11 +0100

    Add Oracle Services

--- model/oracle_database_service.go
+++ model/oracle_database_service.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 Sorint.lab S.p.A.
+// Copyright (c) 2021 Sorint.lab S.p.A.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -16,33 +16,16 @@
 package model
 
 import (
-	"reflect"
-
-	godynstruct "github.com/amreo/go-dyn-struct"
+	"time"
 )
 
 // OracleDatabaseService holds information about an Oracle database service
 type OracleDatabaseService struct {
-	Name      string                 `json:"name" bson:"name"`
-	OtherInfo map[string]interface{} `json:"-" bson:"-"`
-}
-
-// MarshalJSON return the JSON rappresentation of this
-func (v OracleDatabaseService) MarshalJSON() ([]byte, error) {
-	return godynstruct.DynMarshalJSON(reflect.ValueOf(v), v.OtherInfo, "OtherInfo")
-}
-
-// UnmarshalJSON parse the JSON content in data and set the fields in v appropriately
-func (v *OracleDatabaseService) UnmarshalJSON(data []byte) error {
-	return godynstruct.DynUnmarshalJSON(data, reflect.ValueOf(v), &v.OtherInfo, "OtherInfo")
-}
-
-// MarshalBSON return the BSON rappresentation of this
-func (v OracleDatabaseService) MarshalBSON() ([]byte, error) {
-	return godynstruct.DynMarshalBSON(reflect.ValueOf(v), v.OtherInfo, "OtherInfo")
-}
-
-// UnmarshalBSON parse the BSON content in data and set the fields in v appropriately
-func (v *OracleDatabaseService) UnmarshalBSON(data []byte) error {
-	return godynstruct.DynUnmarshalBSON(data, reflect.ValueOf(v), &v.OtherInfo, "OtherInfo")
+	Name            *string    `json:"name" bson:"name,omitempty"`
+	CreationDate    *time.Time `json:"creationDate" bson:"creationDate,omitempty"`
+	FailoverMethod  *string    `json:"failoverMethod" bson:"failoverMethod,omitempty"`
+	FailoverType    *string    `json:"failoverType" bson:"failoverType,omitempty"`
+	FailoverRetries *int       `json:"failoverRetries" bson:"failoverRetries,omitempty"`
+	FailoverDelay   *int       `json:"failoverDelay" bson:"failoverDelay,omitempty"`
+	Enabled         *bool      `json:"enabled" bson:"enabled,omitempty"`
 }
