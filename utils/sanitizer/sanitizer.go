7809d746fcf81bddf1d77ab4503db34f68ba543b
Fabrizio Cirelli <cirelli94@gmail.com>
2021-12-15 15:35:35 +0100

    Added sanitizer

---
+++ utils/sanitizer/sanitizer.go
@@ -0,0 +1,134 @@
+package sanitizer
+
+import (
+	"fmt"
+	"reflect"
+	"time"
+
+	"github.com/microcosm-cc/bluemonday"
+	"github.com/pmezard/go-difflib/difflib"
+
+	"github.com/ercole-io/ercole/v2/logger"
+)
+
+type Sanitizer struct {
+	log logger.Logger
+
+	policy *bluemonday.Policy
+}
+
+func NewSanitizer(log logger.Logger) *Sanitizer {
+	return &Sanitizer{
+		log:    log,
+		policy: bluemonday.StrictPolicy(),
+	}
+}
+
+func (s *Sanitizer) Sanitize(obj interface{}) (interface{}, error) {
+	original := reflect.ValueOf(obj)
+
+	copy := reflect.New(original.Type()).Elem()
+	if err := s.sanitizeRecursive(copy, original); err != nil {
+		return nil, err
+	}
+
+	return copy.Interface(), nil
+}
+
+func (s *Sanitizer) sanitizeRecursive(copy, original reflect.Value) error {
+	switch original.Kind() {
+
+	case reflect.Ptr:
+		// Unwrap and call once again
+
+		originalValue := original.Elem()
+		if !originalValue.IsValid() {
+			return nil // pointer is nil
+		}
+
+		copy.Set(reflect.New(originalValue.Type()))
+		if err := s.sanitizeRecursive(copy.Elem(), originalValue); err != nil {
+			return err
+		}
+
+	case reflect.Interface:
+		originalValue := original.Elem()
+
+		copyValue := reflect.New(originalValue.Type()).Elem()
+		if err := s.sanitizeRecursive(copyValue, originalValue); err != nil {
+			return err
+		}
+		copy.Set(copyValue)
+
+	case reflect.Struct:
+
+		if original.CanInterface() {
+			i := original.Interface()
+
+			// If it is a time.Time, copy directly entire struct
+			if _, ok := i.(time.Time); ok {
+				copy.Set(original)
+				return nil
+			}
+		}
+
+		for i := 0; i < original.NumField(); i += 1 {
+			if err := s.sanitizeRecursive(copy.Field(i), original.Field(i)); err != nil {
+				return err
+			}
+		}
+
+	case reflect.Slice:
+		copy.Set(reflect.MakeSlice(original.Type(), original.Len(), original.Cap()))
+		for i := 0; i < original.Len(); i += 1 {
+			if err := s.sanitizeRecursive(copy.Index(i), original.Index(i)); err != nil {
+				return err
+			}
+		}
+
+	case reflect.Map:
+		copy.Set(reflect.MakeMap(original.Type()))
+		for _, key := range original.MapKeys() {
+			originalValue := original.MapIndex(key)
+			copyValue := reflect.New(originalValue.Type()).Elem()
+
+			if err := s.sanitizeRecursive(copyValue, originalValue); err != nil {
+				return err
+			}
+			copy.SetMapIndex(key, copyValue)
+		}
+
+	case reflect.String:
+		sanitizedString := s.sanitizeString(original.Interface().(string))
+		copy.SetString(sanitizedString)
+
+	default:
+		if !copy.CanSet() {
+			return fmt.Errorf("Can't set value")
+		}
+		copy.Set(original)
+	}
+
+	return nil
+}
+
+func (s *Sanitizer) sanitizeString(a string) string {
+	b := s.policy.Sanitize(a)
+
+	if a != b {
+		diff := difflib.UnifiedDiff{
+			A:        difflib.SplitLines(a),
+			FromFile: "Original",
+
+			B:      difflib.SplitLines(b),
+			ToFile: "Sanitized",
+
+			Context: 3,
+		}
+		text, _ := difflib.GetUnifiedDiffString(diff)
+
+		s.log.Warnf("A string has been sanitized:\n%s", text)
+	}
+
+	return b
+}
