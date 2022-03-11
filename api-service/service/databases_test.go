95af3e3aae42f6be1c3624205d56f4fdf8c5b44b
cbiglioli <cbiglioli@gmail.com>
2022-03-11 12:10:56 +0100

    Changed covered management in compliance

--- api-service/service/databases_test.go
+++ api-service/service/databases_test.go
@@ -2067,7 +2067,7 @@ func TestGetDatabaseLicensesCompliance_Success(t *testing.T) {
 			Purchased:       450,
 			Compliance:      1,
 			Unlimited:       false,
-			Available:       0,
+			Available:       200,
 		},
 		{
 			LicenseTypeID:   "",
