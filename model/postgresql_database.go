e2439ba483ae93433a1abacab44c119bc8b36e6c
afumagalli <afumagalli@sorint.it>
2022-06-06 11:13:14 +0200

    Updated psql models

--- model/postgresql_database.go
+++ model/postgresql_database.go
@@ -16,19 +16,23 @@
 package model
 
 type PostgreSQLDatabase struct {
-	DbSize            string `json:"dbSize" bson:"dbSize"`
-	DbConnections     string `json:"dbConnections" bson:"dbConnections"`
-	TableCount        int    `json:"tableCount" bson:"tableCount"`
-	IndexCount        int    `json:"indexCount" bson:"indexCount"`
-	TablesSize        string `json:"tablesSize" bson:"tablesSize"`
-	IndexesSize       string `json:"indexesSize" bson:"indexesSize"`
-	MviewsSize        string `json:"mviewsSize" bson:"mviewsSize"`
-	ExtensionCount    int    `json:"extensionCount" bson:"extensionCount"`
-	SchemaCount       int    `json:"schemaCount" bson:"schemaCount"`
-	LogicReplSetup    bool   `json:"logicReplSetup" bson:"logicReplSetup"`
-	PublicationCount  int    `json:"publicationCount" bson:"publicationCount"`
-	SubscriptionCount int    `json:"subscriptionCount" bson:"subscriptionCount"`
-	LockCount         int    `json:"lockCount" bson:"lockCount"`
-	MviewCount        int    `json:"mviewCount" bson:"mviewCount"`
-	ViewCount         int    `json:"viewCount" bson:"viewCount"`
+	DbName             string             `json:"dbName" bson:"dbName"`
+	DbOwner            string             `json:"dbOwner" bson:"dbOwner"`
+	Datconnlimit       int                `json:"datconnlimit" bson:"datconnlimit"`
+	SchemasCount       int                `json:"schemasCount" bson:"schemasCount"`
+	DbSize             int                `json:"dbSize" bson:"dbSize"`
+	TablesCount        int                `json:"tablesCount" bson:"tablesCount"`
+	TablesSize         int                `json:"tablesSize" bson:"tablesSize"`
+	IndexesCount       int                `json:"indexesCount" bson:"indexesCount"`
+	IndexesSize        int                `json:"indexesSize" bson:"indexesSize"`
+	MatviewsCount      int                `json:"matviewsCount" bson:"matviewsCount"`
+	MatviewsSize       int                `json:"matviewsSize" bson:"matviewsSize"`
+	ExtensionsCount    int                `json:"extensionsCount" bson:"extensionsCount"`
+	LobsCount          int                `json:"lobsCount" bson:"lobsCount"`
+	LobsSize           int                `json:"lobsSize" bson:"lobsSize"`
+	ViewsCount         int                `json:"viewsCount" bson:"viewsCount"`
+	LogicReplSetup     bool               `json:"logicReplSetup" bson:"logicReplSetup"`
+	PublicationsCount  int                `json:"publicationsCount" bson:"publicationsCount"`
+	SubscriptionsCount int                `json:"subscriptionsCount" bson:"subscriptionsCount"`
+	Schemas            []PostgreSQLSchema `json:"schemas" bson:"schemas"`
 }
