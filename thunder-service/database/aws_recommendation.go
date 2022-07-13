ebeabe365a359cb39a2c358a050ad1f0355f0b3f
afumagalli <afumagalli@sorint.it>
2022-07-13 10:40:38 +0200

    Added aws object

---
+++ thunder-service/database/aws_recommendation.go
@@ -0,0 +1,40 @@
+// Copyright (c) 2022 Sorint.lab S.p.A.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package database
+
+import (
+	"context"
+
+	"github.com/ercole-io/ercole/v2/utils"
+)
+
+func (md *MongoDatabase) AddAwsObject(m interface{}, collection string) error {
+	_, err := md.Client.Database(md.Config.Mongodb.DBName).Collection(collection).InsertOne(context.TODO(), m)
+	if err != nil {
+		return utils.NewError(err, "DB ERROR")
+	}
+
+	return nil
+}
+
+func (md *MongoDatabase) AddAwsObjects(m []interface{}, collection string) error {
+	_, err := md.Client.Database(md.Config.Mongodb.DBName).Collection(collection).InsertMany(context.TODO(), m)
+	if err != nil {
+		return utils.NewError(err, "DB ERROR")
+	}
+
+	return nil
+}
