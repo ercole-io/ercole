9218183380b431de703d1393affda7e1960ed9b7
afumagalli <afumagalli@sorint.it>
2022-07-25 11:23:13 +0200

    Retrieve Compute Instance Rightsizing

--- thunder-service/job/aws_data_retrieve_job.go
+++ thunder-service/job/aws_data_retrieve_job.go
@@ -87,6 +87,12 @@ func (job *AwsDataRetrieveJob) Run() {
 				c <- err
 			}
 		}(profile, seqValue)
+
+		go func(profile model.AwsProfile, seq uint64) {
+			if err := job.FetchAwsComputeInstanceRightsizing(profile, seq); err != nil {
+				c <- err
+			}
+		}(profile, seqValue)
 	}
 
 	job.Log.Error(<-c)
