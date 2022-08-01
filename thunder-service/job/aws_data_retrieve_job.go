e3e0bf9ac8eebb4590a381265918edb6e12220c3
cbiglioli <cbiglioli@gmail.com>
2022-08-01 17:21:07 +0200

    Retrieve data from AWS for Compute Instance Decommisioning 2

--- thunder-service/job/aws_data_retrieve_job.go
+++ thunder-service/job/aws_data_retrieve_job.go
@@ -93,6 +93,12 @@ func (job *AwsDataRetrieveJob) Run() {
 				c <- err
 			}
 		}(profile, seqValue)
+
+		go func(profile model.AwsProfile, seq uint64) {
+			if err := job.FetchAwsInstanceDecommissioning2(profile, seq); err != nil {
+				c <- err
+			}
+		}(profile, seqValue)
 	}
 
 	job.Log.Error(<-c)
