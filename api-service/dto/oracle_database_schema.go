26d0c85d536db6d888575183ff49ba9839f787f5
afumagalli <afumagalli@sorint.it>
2022-08-24 16:15:11 +0200

    Added database name to schema & swagger

--- api-service/dto/oracle_database_schema.go
+++ api-service/dto/oracle_database_schema.go
@@ -1,10 +1,11 @@
 package dto
 
 type OracleDatabaseSchema struct {
-	Hostname string `json:"hostname" bson:"hostname"`
-	Indexes  int    `json:"indexes" bson:"indexes"`
-	LOB      int    `json:"lob" bson:"lob"`
-	Tables   int    `json:"tables" bson:"tables"`
-	Total    int    `json:"total" bson:"total"`
-	User     string `json:"user" bson:"user"`
+	Hostname     string `json:"hostname" bson:"hostname"`
+	DatabaseName string `json:"databaseName" bson:"databaseName"`
+	Indexes      int    `json:"indexes" bson:"indexes"`
+	LOB          int    `json:"lob" bson:"lob"`
+	Tables       int    `json:"tables" bson:"tables"`
+	Total        int    `json:"total" bson:"total"`
+	User         string `json:"user" bson:"user"`
 }
