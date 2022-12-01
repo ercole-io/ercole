859d5a36a818087b88301a2df3e42cd6f257659c
cbiglioli <cbiglioli@gmail.com>
2022-12-01 11:39:23 +0100

    Retrieve schemas status from Oracle Database

--- api-service/dto/oracle_database_schema.go
+++ api-service/dto/oracle_database_schema.go
@@ -1,11 +1,27 @@
+// Copyright (c) 2022 Sorint.lab S.p.A.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 package dto
 
 type OracleDatabaseSchema struct {
-	Hostname     string `json:"hostname" bson:"hostname"`
-	DatabaseName string `json:"databaseName" bson:"databaseName"`
-	Indexes      int    `json:"indexes" bson:"indexes"`
-	LOB          int    `json:"lob" bson:"lob"`
-	Tables       int    `json:"tables" bson:"tables"`
-	Total        int    `json:"total" bson:"total"`
-	User         string `json:"user" bson:"user"`
+	Hostname      string `json:"hostname" bson:"hostname"`
+	DatabaseName  string `json:"databaseName" bson:"databaseName"`
+	Indexes       int    `json:"indexes" bson:"indexes"`
+	LOB           int    `json:"lob" bson:"lob"`
+	Tables        int    `json:"tables" bson:"tables"`
+	Total         int    `json:"total" bson:"total"`
+	User          string `json:"user" bson:"user"`
+	AccountStatus string `json:"accountStatus" bson:"accountStatus"`
 }
