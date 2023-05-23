e0bc2a69f70e43c24860df43575f56aac4fa8f94
afumagalli98 <a.fuma.98@gmail.com>
2023-05-23 15:49:33 +0200

    Added memory & cpu data to exadata API

---
+++ api-service/dto/oracle_exadata_component.go
@@ -0,0 +1,48 @@
+// Copyright (c) 2023 Sorint.lab S.p.A.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package dto
+
+import "github.com/ercole-io/ercole/v2/model"
+
+type OracleExadataComponent struct {
+	model.OracleExadataComponent
+	UsedRAM int `json:"usedRAM"`
+	FreeRAM int `json:"freeRAM"`
+	UsedCPU int `json:"usedCPU"`
+	FreeCPU int `json:"freeCPU"`
+}
+
+func ToOracleExadataComponent(componentModel *model.OracleExadataComponent) OracleExadataComponent {
+	if componentModel != nil {
+		res := OracleExadataComponent{OracleExadataComponent: *componentModel}
+
+		for _, vm := range componentModel.VMs {
+			if vm.Type == model.VM_KVM || vm.Type == model.VM_XEN {
+				res.UsedRAM += vm.RamCurrent + vm.RamOnline
+				res.UsedCPU += vm.CPUCurrent + vm.CPUOnline
+			}
+		}
+
+		if res.HostType == model.DOM0 || res.HostType == model.KVM_HOST {
+			res.FreeRAM = res.Memory - res.UsedRAM
+			res.FreeCPU = res.TotalCPU - res.UsedCPU
+		}
+
+		return res
+	}
+
+	return OracleExadataComponent{}
+}
---
+++ api-service/dto/oracle_exadata_instance.go
@@ -0,0 +1,72 @@
+// Copyright (c) 2023 Sorint.lab S.p.A.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+package dto
+
+import "github.com/ercole-io/ercole/v2/model"
+
+type OracleExadataInstance struct {
+	Hostname    string                   `json:"hostname"`
+	RackID      string                   `json:"rackID"`
+	Components  []OracleExadataComponent `json:"components"`
+	TotalMemory int                      `json:"totalMemory"`
+	UsedMemory  int                      `json:"usedMemory"`
+	FreeMemory  int                      `json:"freeMemory"`
+	TotalCPU    int                      `json:"totalCPU"`
+	UsedCPU     int                      `json:"usedCPU"`
+	FreeCPU     int                      `json:"freeCPU"`
+}
+
+func ToOracleExadataInstance(inst *model.OracleExadataInstance) OracleExadataInstance {
+	if inst != nil {
+		res := OracleExadataInstance{
+			Hostname: inst.Hostname,
+			RackID:   inst.RackID,
+		}
+
+		for _, cmp := range inst.Components {
+			if cmp.HostType == model.DOM0 || cmp.HostType == model.KVM_HOST {
+				res.TotalMemory += cmp.Memory
+				res.TotalCPU += cmp.TotalCPU
+
+				for _, vm := range cmp.VMs {
+					if vm.Type == model.VM_KVM || vm.Type == model.VM_XEN {
+						res.UsedMemory += vm.RamCurrent + vm.RamOnline
+						res.UsedCPU += vm.CPUCurrent + vm.CPUOnline
+					}
+				}
+			}
+
+			res.Components = append(res.Components, ToOracleExadataComponent(&cmp))
+		}
+
+		res.FreeMemory = res.TotalMemory - res.UsedMemory
+		res.FreeCPU = res.TotalCPU - res.UsedCPU
+
+		return res
+	}
+
+	return OracleExadataInstance{}
+}
+
+func ToOracleExadataInstances(instancesModel []model.OracleExadataInstance) []OracleExadataInstance {
+	res := make([]OracleExadataInstance, 0, len(instancesModel))
+
+	for _, instance := range instancesModel {
+		res = append(res, ToOracleExadataInstance(&instance))
+	}
+
+	return res
+}
