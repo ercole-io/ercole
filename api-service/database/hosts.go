c19919c778f24d6a04f0bc09a82275705d202e6b
afumagalli98 <a.fuma.98@gmail.com>
2023-06-06 15:54:40 +0200

    Ignoring dismissed hosts & alerts

--- api-service/database/hosts.go
+++ api-service/database/hosts.go
@@ -906,6 +906,7 @@ func (md *MongoDatabase) IsMissingDB(hostname string) (bool, error) {
 			context.TODO(), bson.M{
 				"alertCode":          model.AlertCodeMissingDatabase,
 				"otherInfo.hostname": hostname,
+				"alertStatus":        bson.M{"$ne": "DISMISSED"},
 			})
 	if err != nil {
 		return false, utils.NewError(err, "DB ERROR")
